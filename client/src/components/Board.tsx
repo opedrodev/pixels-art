98f0a0e72e00b7a82087802186831dd797bfc103
opedrodev <pvasilva.dev@hotmail.com>
2023-03-10 16:24:29 -0300

    feat: update board styles

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -8,7 +8,7 @@ interface Props {
 function Board({ pixels, size }: Props) {
   return (
     <div
-      className='w-fit h-fit grid grid-cols-[repeat(20,_1fr)] gap-[1px]'
+      className='w-fit h-fit grid grid-cols-[repeat(20,_1fr)] justify-self-center self-center border border-gray-200'
       onContextMenu={ (event) => event.preventDefault() }
       id='board'
     >

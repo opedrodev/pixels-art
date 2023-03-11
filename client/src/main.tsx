d0140aeb0ab44806e8395ef9660652b35edb2822
opedrodev <pvasilva.dev@hotmail.com>
2023-03-11 00:59:55 -0300

    feat: add react router dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import store from 'app/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );

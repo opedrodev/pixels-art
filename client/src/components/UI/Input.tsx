8671c9d97c067d8bd5f255afa1d83085e740b4c1
opedrodev <pvasilva.dev@hotmail.com>
2023-03-12 15:12:54 -0300

    feat: toggle show password added

--- client/src/components/UI/Input.tsx
+++ client/src/components/UI/Input.tsx
@@ -1,4 +1,5 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
+import { BiHide, BiShow } from 'react-icons/bi';
 
 interface Input extends InputHTMLAttributes<HTMLInputElement> {
   type: string;
@@ -6,6 +7,32 @@ interface Input extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Input({ type, className, ...props }: Input) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  if (type === 'password') {
+    return (
+      <div className='relative'>
+        <input
+          type={ showPassword ? 'text' : 'password' }
+          { ...props }
+          className={ `w-full py-1 px-2 pr-8 border-b border-gray-200 focus:border-black outline-none ${className}` }
+        />
+
+        { showPassword ? (
+          <BiHide
+            className='absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-400 cursor-pointer text-xl select-none'
+            onClick={ () => setShowPassword(false) }
+          />
+        ) : (
+          <BiShow
+            className='absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-400 cursor-pointer text-xl select-none'
+            onClick={ () => setShowPassword(true) }
+          />
+        ) }
+      </div>
+    );
+  }
+
   return (
     <input
       type={ type }

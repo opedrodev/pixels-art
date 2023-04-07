4278a10b7c0f3ef5ea88bdffa36d6c8aa8fde815
opedrodev <pvasilva.dev@hotmail.com>
2023-04-07 00:53:31 -0300

    feat: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 import UserRouter from './routes/users.routes';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/health', async (req: Request, res: Response) => {
   res.send('OK');

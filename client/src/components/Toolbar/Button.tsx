c9245085b032617ec4b058a6a6aeba11333e3567
opedrodev <pvasilva.dev@hotmail.com>
2023-04-08 22:27:12 -0300

    feat: add home and save button

--- client/src/components/Toolbar/Button.tsx
+++ client/src/components/Toolbar/Button.tsx
@@ -4,11 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 
 interface Button {
   icon: ReactNode;
-  type: 'pencil' | 'eraser' | 'picker';
+  type: 'pencil' | 'eraser' | 'picker' | 'save' | 'home';
   title: string;
+  onClick?: () => void;
 }
 
-function Button({ icon, type, title }: Button) {
+function Button({ icon, type, title, ...props }: Button) {
   const { activeTool } = useSelector((state: ToolbarState) => state.toolbar);
   const dispatch = useDispatch();
 
@@ -16,8 +17,9 @@ function Button({ icon, type, title }: Button) {
     <button
       className='w-fit h-fit p-4 border border-gray-200 hover:bg-gray-200'
       style={ { backgroundColor: activeTool === type ? 'rgb(229, 231, 235)' : '' } }
-      onClick={ () => dispatch(setActiveTool(activeTool === type ? '' : type)) }
       title={ title }
+      onClick={ () => dispatch(setActiveTool(activeTool === type ? '' : type)) }
+      { ...props }
     >
       { icon }
     </button>
---
+++ client/src/components/Toolbar/index.tsx
@@ -0,0 +1,60 @@
+import { RiSaveLine } from 'react-icons/ri';
+import { TbColorPicker, TbEraser, TbHome, TbPencil } from 'react-icons/tb';
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+import ColorPicker from './ColorPicker';
+
+function Toolbar() {
+  const navigate = useNavigate();
+
+  function onSaveBoard() {
+    console.log('Save');
+  }
+
+  return (
+    <section className='grid grid-rows-[auto_1fr_auto] shadow-sm h-[100vh] border-r border-r-gray-200'>
+      <ColorPicker />
+
+      <div className='grid grid-cols-[auto_auto] justify-center content-start gap-8 mt-8'>
+        <Button
+          type='pencil'
+          icon={ <TbPencil className='text-xl' /> }
+          title='Pencil'
+        />
+
+        <Button
+          type='eraser'
+          icon={ <TbEraser className='text-xl' /> }
+          title='Eraser'
+        />
+
+        <Button
+          type='picker'
+          icon={ <TbColorPicker className='text-xl' /> }
+          title='Color Picker'
+        />
+
+      </div>
+
+      <div className='grid grid-cols-[auto_auto] justify-center gap-8 py-8 border-t border-gray-200'>
+        <Button
+          type='home'
+          icon={ <TbHome className='text-xl' /> }
+          title='Home'
+          onClick={ () => navigate('/home') }
+        />
+
+        <Button
+          type='save'
+          icon={ <RiSaveLine className='text-xl' /> }
+          title='Save'
+          onClick={ onSaveBoard }
+        />
+      </div>
+    </section>
+  );
+}
+
+
+
+export default Toolbar;
